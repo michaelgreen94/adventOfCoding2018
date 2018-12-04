9db81753c9303a5da8c37ac7a98a2e57ac75bf56
michael <michael.green94@hotmail.com>
2018-12-03 22:59:35 -0700

    setting up advent front end

---
+++ assets/snowflake.js
@@ -0,0 +1,11 @@
+class Snowflake {
+
+  constructor() {
+    //snowflake position
+    this.pos = createVector();
+    //snowflake velocity
+    this.vel = createVector();
+    //snowflake acceleration
+    this.acc = createVector();
+  }
+}
