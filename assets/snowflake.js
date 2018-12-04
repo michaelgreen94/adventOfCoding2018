3e34229456095c1d19e9d3fb4e05be373ddce939
michael <michael.green94@hotmail.com>
2018-12-04 00:51:22 -0700

    spinning flakes!

--- assets/snowflake.js
+++ assets/snowflake.js
@@ -1,7 +1,7 @@
 function getRandomSize() {
 
   let r = pow(random(0, 1), 3);
-  return constrain(r * 32, 2, 32);
+  return constrain(r * 32, 8, 32);
 
   // let r = randomGaussian() * 2;
   // return constrain(abs(r * r), 2, 36);
@@ -28,20 +28,21 @@ class Snowflake {
     this.vel = createVector(0, 0);
     //snowflake acceleration
     this.acc = createVector();
+    this.angle = random(TWO_PI)
     //snowflake size
     this.r = getRandomSize();
     //terminal velocity
-    this.terminal = this.r
+    // this.terminal = this.r
   }
 
   applyForce(force) {
     //parallax effect hack
     let f = force.copy();
-    f.mult(this.r * 0.3)
+    f.mult(this.r)
 
     // let f = force.copy();
     // f.div(this.mass);
-    this.acc.add(force);
+    this.acc.add(f);
   }
 
   randomize() {
@@ -57,11 +58,13 @@ class Snowflake {
 
   update() {
     //adds velocity to the position
-    this.vel.limit(this.r)
+    this.vel.add(this.acc);
+    this.vel.limit(this.r * 0.2)
+
     if (this.vel.mag() < 1) {
       this.vel.normalize();
     }
-    this.vel.add(this.acc);
+
     this.pos.add(this.vel)
     this.acc.mult(0);
 
@@ -74,8 +77,15 @@ class Snowflake {
     // stroke(255);
     // strokeWeight(this.r);
     // point(this.pos.x, this.pos.y)
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.angle);
     imageMode(CENTER);
-    image(this.img, this.pos.x, this.pos.y, this.r, this.r)
+    image(this.img, 0, 0, this.r, this.r)
+    pop();
+
+    //rotates snowflakes by the velocities magnitude
+    this.angle += this.vel.mag() / 150;
   }
 
   // offScreen() {
